2ed9d3e3dc260d95dff8bfc345e869245b1777a2
gtibap <gtibap@gmail.com>
2023-08-29 17:07:57 -0400

    Add files via upload

---
+++ scripts/class_emg_filtering.py
@@ -0,0 +1,224 @@
+# import kineticstoolkit.lab as ktk
+import scipy.io
+from scipy import signal
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import sys
+
+class Reading_EMG:
+
+    def __init__(self, path, filename, ids_channels):
+
+        self.path=''
+        self.filename=[]
+        self.sampling_rate=1.0
+        self.n_channels=1
+        self.channels=[]
+        self.channelsNames=[]
+        
+        self.path = path
+        self.filename = filename
+        # self.day_number = day_number
+    
+        mat = scipy.io.loadmat(self.path+self.filename)
+        print(f'\n{self.filename}')
+        # print(f'dictionary {mat}')
+        print('Header:',  mat['__header__'])
+        print('Channel Names:',  mat['channelNames'])
+        
+        self.sampling_rate = mat['samplingRate'][0,0]
+        print(f'sample rate: {self.sampling_rate}')
+        # ## number of channels plus one to include the Time channel (channel 0)
+        
+        # self.n_channels = mat['noChans'][0,0]+1
+        # self.n_channels = 9
+        
+        # print(f'self.n_channels {self.n_channels}')
+        
+        self.n_channels = ids_channels[1]-ids_channels[0] + 1
+        
+        self.channels = np.empty((self.n_channels, 0)).tolist()
+        self.channelsNames = np.empty((self.n_channels, 0)).tolist()
+
+        ## channel 0 is the time array
+        self.ch_time = mat['Data'][0, 0].flatten()
+        self.ch_time_name = mat['channelNames'][0][0][0]
+        # print(f'time ch: {self.ch_time.shape}')
+        
+        i=0
+        for num_ch in np.arange(ids_channels[0], ids_channels[1]+1):
+            self.channels[i] = mat['Data'][0, num_ch].flatten()
+            self.channelsNames[i] = mat['channelNames'][0][num_ch][0]
+            i+=1
+
+        # print(f'channels: {len(self.channels)}, {len(self.channels[0])}, {len(self.channelsNames)}')
+
+
+    def smoothingRMS(self, ch, window_size):
+        ## window_size is in miliseconds
+        # spm = 60 ## seconds per min
+        # window_size = int(spm*self.window_min)
+        ## transform window size to number of samples
+        window_samples = int(self.sampling_rate * window_size*1e-3)
+        # print(f'window_samples: {window_samples}')
+        
+        # print(f'window: {window}')
+        # print(  'window size (s): ', window_size)
+        ## window to average values (same weight)
+        
+        window = np.ones(window_samples)/(window_samples)
+        ch_smoothed = np.sqrt(signal.convolve(ch**2, window, 'same'))
+
+        # window = signal.windows.boxcar(window_samples)
+        # ch_smoothed = np.rint(signal.convolve(ch, window, mode='same'))/window_samples
+        
+        
+        return ch_smoothed
+        
+        
+    def filterHighPass(self, emg, fc):
+        sos = signal.butter(10, fc, btype='highpass', fs=self.sampling_rate, output='sos')
+        filtered = signal.sosfilt(sos, emg)
+        return filtered
+
+    def filterLowPass(self, emg, fc):
+        sos = signal.butter(10, fc, btype='lowpass', fs=self.sampling_rate, output='sos')
+        filtered = signal.sosfilt(sos, emg)
+        return filtered
+
+    def filterBandPass(self, emg, fc1, fc2):
+        sos = signal.butter(10, [fc1,fc2], btype='bandpass', fs=self.sampling_rate, output='sos')
+        filtered = signal.sosfilt(sos, emg)
+        return filtered
+        
+        
+    # def smoothingRMS(self, window_size):
+        
+        # ## window size in mili-seconds
+        # window_samples = np.rint(self.sampling_rate * window_size*1e-3)
+        # print('sampling_rate, window_samples: ', self.sampling_rate, window_samples)
+        # window = np.ones(window_samples)/float(window_samples)
+        # ## we excluded 'Time' and 'Switch' channels (first and last channels)
+        # self.channels_rms = np.empty((self.n_channels-2, 0)).tolist()
+        # i=0
+        # for ch in self.channels[1:-1]:
+            # ## RMS window smoothing
+            # self.channels_rms[i] = np.sqrt(np.convolve(ch**2, window, 'same'))
+            # i+=1
+        
+        # return 0
+        
+
+    def plotSignals(self):
+        # print(f'\nids channels: {ids_channels}')
+        fig, ax = plt.subplots(nrows=8, ncols=1, sharex=True, sharey=True)
+        fig.canvas.mpl_connect('key_press_event', self.on_press)
+        
+        cont=0
+        for ch, ch_n in zip(self.channels, self.channelsNames):
+            ax[cont].plot(self.ch_time, ch, label=ch_n)
+            ax[cont].legend()
+            cont+=1
+        
+        ax[0].set_ylim([-500,500])
+        ax[0].set_title(self.filename)
+        ax[cont-1].set_xlabel(self.ch_time_name+' [s]')
+
+        return 0
+        
+    def plotSelectedSignal(self, signal_number):
+        # print(f'\nids channels: {ids_channels}')
+        fig, ax = plt.subplots(nrows=2, ncols=2)
+        fig.canvas.mpl_connect('key_press_event', self.on_press)
+        
+        # cont=0
+        ch = self.channels[signal_number]
+        ch_n = self.channelsNames[signal_number]
+        # for ch, ch_n in zip(self.channels, self.channelsNames):
+        
+        # window_size=20 ## miliseconds
+        # ch_s = self.smoothingRMS(ch, window_size)
+        # filtered_ch = ktk.filters.butter(ch, btype="bandpass", fc=[20, 500])
+        fc1 = 50 ## 20 Hz high pass filter
+        fc2 = 500 ## Hz
+        ch_s = self.filterBandPass(ch, fc1, fc2)
+
+        f1, Pxx_spec1 = signal.periodogram(ch, self.sampling_rate, 'flattop', scaling='spectrum')
+        f2, Pxx_spec2 = signal.periodogram(ch_s, self.sampling_rate, 'flattop', scaling='spectrum')
+        
+        ax[0][0].plot(self.ch_time, ch, label=ch_n)
+        ax[0][0].legend()
+        
+        ax[0][1].plot(self.ch_time, ch_s, label=ch_n)
+        ax[0][1].legend()
+        
+        ax[1][0].axes.semilogy(f1, np.sqrt(Pxx_spec1),label=ch_n)
+        ax[1][0].legend()
+        
+        ax[1][1].axes.semilogy(f2, np.sqrt(Pxx_spec2),label=ch_n)
+        ax[1][1].legend()
+
+        id01 = (len(self.ch_time)/2 - (self.sampling_rate*2.5)).astype(int)
+        id02 = (id01 + (self.sampling_rate*5)).astype(int)        
+        
+        ax[0][0].set_xlim([self.ch_time[id01],self.ch_time[id02]])
+        ax[0][1].set_xlim([self.ch_time[id01],self.ch_time[id02]])
+        
+        amp_y = 100
+        
+        ax[0][0].set_ylim([-amp_y,amp_y])
+        ax[0][1].set_ylim([-amp_y,amp_y])
+        ax[1][0].set_ylim([10e-8, 10e0])
+        ax[1][1].set_ylim([10e-8, 10e0])
+        
+        ax[0][0].set_title(self.filename+' (original)')
+        ax[0][1].set_title(f'{self.filename} passband {fc1} Hz - {fc2} Hz')
+        
+        ax[0][0].set_xlabel(self.ch_time_name+' [s]')
+        ax[0][1].set_xlabel(self.ch_time_name+' [s]')
+        ax[1][0].set_xlabel('frequency [Hz]')
+        ax[1][1].set_xlabel('frequency [Hz]')
+        
+        ax[0][0].set_ylabel('magnitude')
+        ax[0][1].set_ylabel('magnitude')
+        ax[1][0].set_ylabel('magnitude')
+        ax[1][1].set_ylabel('magnitude')
+
+        return 0
+        
+    # def plotPowerSpectrum(self):
+        
+        # time = self.channels[0]
+        # time_label = self.channelsNames[0]
+        # ## we exclude Time channel and Swich channel (first and last channels)
+        # fig, ax = plt.subplots(nrows=(self.n_channels-2), ncols=1, sharex=True, sharey=True)
+        # fig.canvas.mpl_connect('key_press_event', self.on_press)
+        # cont=0
+        # for ch, ch_n in zip(self.channels[1:-1], self.channelsNames[1:-1]):
+            # f, Pxx_spec = signal.periodogram(ch, self.sampling_rate, 'flattop', scaling='spectrum')
+            # ax[cont].axes.semilogy(f, np.sqrt(Pxx_spec),label=ch_n)
+            # ax[cont].legend()
+            # cont+=1
+        
+        # ax[0].set_title('Linear spectrum [V RMS] '+self.filename)
+        # ax[cont-1].set_xlabel('frequency [Hz]')
+            
+        # return 0
+        
+    def on_press(self, event):
+        # print('press', event.key)
+        sys.stdout.flush()
+        
+        if event.key == 'x':
+            plt.close('all')
+        else:
+            pass
+        return 0
+        
+    def getChannelsNames(self):
+        return self.channelsNames
+        
+        
+        
+    
---
+++ scripts/emg_filtering.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  emg_filtering.py
+#  
+
+import argparse
+from class_emg_filtering import Reading_EMG
+import matplotlib.pyplot as plt
+import sys
+
+def main(args):
+    
+    # Initialize parser
+    parser = argparse.ArgumentParser(description = 'EMG filtering')
+
+    # Adding optional argument
+    parser.add_argument('-d', '--dir_name', type = str, help = "Select directory, for example: EBC032/EBC033/")
+    parser.add_argument('-p', '--patient_number', type = str, help = "Select file number, for example: 033")
+    
+    parser.add_argument('-f', '--file_number', type = str, help = "Select file number, for example: 0")
+    
+    parser.add_argument('-s', '--signal_number', type = str, help = "Select signal number, for example: 0")
+    
+    args = parser.parse_args()
+
+    path=args.dir_name
+    patient_number=args.patient_number
+    file_number=int(args.file_number)
+    signal_number=int(args.signal_number)
+    
+    print(f'path and files: {path}, {patient_number}, {file_number}')
+    
+    list_files_names = {'003':['EBC003-J1.mat','EBC003-S7.1.mat','EBC003-S7.2.mat','EBC003-J14.mat'],
+                        
+                        # '037':['EBC37_S1_BASELINE1.mat','EBC037_S2_BASELINE2.mat', 'EBC037_S2_E1.mat','EBC037_S2_E2.mat','EBC037_S2_E3.mat', 'EBC037S7-Baseline1.mat','EBC037S7e1.mat','EBC037S7e2.mat','EBC037S7e3.mat','EBC037_S14_BASELINE1.mat','EBC037_S14_BASELINE2.mat','EBC037_S14_E1.mat','EBC037_S14_E2.mat','EBC039S14E2.mat','EBC037S14E3.mat'],
+                        '003':['EBC003-J1.mat','EBC003-J14.mat'],
+                        '004':['EBC004-S1.mat','EBC004  J13.mat'],
+                        '006':['EBC 006 S1 E3.mat','EBC006 s15 e3.mat'],
+                        '018':['EBC018_S3_E3.mat','EBC018_S12_e3.mat'],
+                        '024':['EBC24-S4-E3.mat','EBC024S13e3.mat'],
+                        '030':['EBC030_S1_E3.mat','EBC30_S14_E3.mat'],
+                        '031':['EBC031_s2_e3.mat','EBC031_s14_e3.mat'],
+                        '032':['EBC032_s1_e3.mat','EBC032S14e3.mat'],
+                        '033':['EBC033S3e3.mat','EBC033S14e3.mat'],
+                        '037':['EBC037_S2_E3.mat','EBC037S14E3.mat'],
+                        # '042':['EB042S730MIN.mat'],
+                        '045':['EBC45-S2-25min.mat','EBC045S14e3.mat'],
+                        # 'test':['test2-config1.mat','test3-config2.mat'],
+                        }
+    
+      
+                        
+    ## ids of the eight required channels: muscular signals EMG without the insole signals
+    list_ids_channels = {'003':[[9,16]]*4,
+                         '006':[[9,16]]*2,
+                         '004':[[9,16]]*2,
+                         '006':[[9,16]]*2,
+                         '018':[[1,8]]*2,
+                         '024':[[9,16]]*2,
+                         '030':[[1,8]]*2,
+                         '031':[[1,8]]*2,
+                         '032':[[1,8]]*2,
+                         '033':[[1,8]]*2,
+                         '037':[[1,8]]*2,
+                         '042':[[1,8]]*2,
+                         '045':[[9,16]]*2,
+                        }
+                        
+    
+                        
+    files = list_files_names[patient_number]
+    ids_channels = list_ids_channels[patient_number]
+    
+    filename = files[file_number]
+    file_channels = ids_channels[file_number]
+    
+    print(f'files: {files}')
+    print(f'channels: {ids_channels}')
+    
+    print(f'selected file: {filename}, channels: {file_channels}')
+    
+    obj_emg = Reading_EMG(path, filename, file_channels)
+    ch_names = obj_emg.getChannelsNames()
+    print(f'names: {ch_names}')
+    
+    obj_emg.plotSignals()
+    obj_emg.plotSelectedSignal(signal_number)
+    
+    # ## open files
+    # list_objs = []
+    # ## read emg-signals from all recordings of a selected session
+    # for num_rec, filename in enumerate(files):
+        # print(f'filename: {filename}')
+        # list_objs.append(Reading_EMG(path,filename, list_ids_channels[file_number][0]))
+    
+    # list_objs[0].plotSignals()
+    
+    plt.ion()
+    plt.show(block=True)
+    
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
